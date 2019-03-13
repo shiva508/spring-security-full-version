4b2025d0870af16cbba6f3ae255165e896cd6cb0
shiva <shiva@pgi-amarendrad1.Prodigy.local>
2019-03-13 13:49:06 +0530

    changes

--- src/main/java/com/demo/controller/LoginController.java
+++ src/main/java/com/demo/controller/LoginController.java
@@ -1,5 +1,5 @@
 package com.demo.controller;
-
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,7 @@ public class LoginController {
 	@GetMapping("/")
 	public String welcome() {
 		return "welcome";
+		
 	}
 	@GetMapping("/loginpage")
 	public String loginPage() {
